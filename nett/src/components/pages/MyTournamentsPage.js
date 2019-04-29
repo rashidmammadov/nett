6fa626d36ae819e8a0cd2e38aff9f0eaba9bac22
Rashid Mammadov <reshid@live.com>
2019-04-29 12:20:56 +0300

    Added google analytics to my tournaments page.

--- nett/src/components/pages/MyTournamentsPage.js
+++ nett/src/components/pages/MyTournamentsPage.js
@@ -6,6 +6,8 @@ import TournamentsList from '../TournamentsList.js';
 import {SUCCESS} from "../../services/Constants";
 import {getMyTournaments} from "../../services/TournamentService";
 import {errorToast, warningToast} from "../../services/ToastService";
+import {style} from "../../assets/style/Custom";
+import {googleTrack} from "../../services/GoogleAnalytics";
 
 export default class StartPage extends Component {
 
@@ -40,6 +42,7 @@ export default class StartPage extends Component {
         if (!this.state.tournaments[index].loaded) {
             let data = {'status': this.state.tournaments[index].status};
             this.setState({loading: true});
+            googleTrack('My Tournaments Page', ('send request to get my tournaments with status: ' + this.state.tournaments[index].status));
             getMyTournaments(data)
                 .then((res) => {
                     this.setState({loading: false});
@@ -48,41 +51,44 @@ export default class StartPage extends Component {
                         d[index].loaded = true;
                         d[index].data = res.data;
                         this.setState({d});
+                        googleTrack('My Tournaments Page', 'response of ' + (this.state.tournaments[index].status) + ' tournaments');
                     } else {
                         warningToast(res.message);
+                        googleTrack('My Tournaments Page', 'warning of ' + (this.state.tournaments[index].status) + ' tournaments', res.message);
                     }
                 })
                 .catch((error) => {
                     this.setState({loading: false});
                     errorToast(error.message);
+                    googleTrack('My Tournaments Page', 'error of ' + (this.state.tournaments[index].status) + ' tournaments', error.message);
                 });
         }
     }
 
     render() {
         return (
-            <View style={{flex: 1}}>
+            <View style={style.flex}>
                 <LoadingDialog loading={this.state.loading}/>
                 <Tabs initialPage={this.state.initialPage} onChangeTab={(tab) => this.getSelectedTabsResult(tab.i)}>
-                    <Tab heading="Aktif" tabStyle={{backgroundColor: '#303030'}}
-                         style={{backgroundColor: '#000'}}
-                         textStyle={{color: '#a3a3a3', fontFamily: 'GoogleSans-Regular', fontWeight: 'normal'}}
-                         activeTabStyle={{backgroundColor: '#303030'}}
-                         activeTextStyle={{color: '#f8f8f8', fontFamily: 'GoogleSans-Regular', fontWeight: 'normal'}}>
+                    <Tab heading="Aktif" tabStyle={style.secondaryBGColor}
+                         style={style.primaryBGColor}
+                         textStyle={style.tabTextStyle}
+                         activeTabStyle={style.secondaryBGColor}
+                         activeTextStyle={style.tabActiveTextStyle}>
                         <TournamentsList loading={!this.state.tournaments[0].loaded} tournaments={this.state.tournaments[0].data} />
                     </Tab>
-                    <Tab heading="Bekleyen" tabStyle={{backgroundColor: '#303030'}}
-                         style={{backgroundColor: '#000'}}
-                         textStyle={{color: '#a3a3a3', fontFamily: 'GoogleSans-Regular', fontWeight: 'normal'}}
-                         activeTabStyle={{backgroundColor: '#303030'}}
-                         activeTextStyle={{color: '#f8f8f8', fontFamily: 'GoogleSans-Regular', fontWeight: 'normal'}}>
+                    <Tab heading="Bekleyen" tabStyle={style.secondaryBGColor}
+                         style={style.primaryBGColor}
+                         textStyle={style.tabTextStyle}
+                         activeTabStyle={style.secondaryBGColor}
+                         activeTextStyle={style.tabActiveTextStyle}>
                         <TournamentsList loading={!this.state.tournaments[1].loaded} tournaments={this.state.tournaments[1].data} />
                     </Tab>
-                    <Tab heading="Bitmiş" tabStyle={{backgroundColor: '#303030'}}
-                         style={{backgroundColor: '#000'}}
-                         textStyle={{color: '#a3a3a3', fontFamily: 'GoogleSans-Regular', fontWeight: 'normal'}}
-                         activeTabStyle={{backgroundColor: '#303030'}}
-                         activeTextStyle={{color: '#f8f8f8', fontFamily: 'GoogleSans-Regular', fontWeight: 'normal'}}>
+                    <Tab heading="Bitmiş" tabStyle={style.secondaryBGColor}
+                         style={style.primaryBGColor}
+                         textStyle={style.tabTextStyle}
+                         activeTabStyle={style.secondaryBGColor}
+                         activeTextStyle={style.tabActiveTextStyle}>
                         <TournamentsList loading={!this.state.tournaments[2].loaded} tournaments={this.state.tournaments[2].data} />
                     </Tab>
                 </Tabs>
