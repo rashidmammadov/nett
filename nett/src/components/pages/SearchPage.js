38c1bbf860ca92a08aebe8c20d6d7a80a7114f5b
Rashid Mammadov <reshid@live.com>
2019-04-29 12:04:25 +0300

    Added google analytics to search tournament page.

--- nett/src/components/pages/SearchPage.js
+++ nett/src/components/pages/SearchPage.js
@@ -5,13 +5,13 @@ import {search} from "../../services/TournamentService";
 import {SUCCESS, TOURNAMENT_STATUS_OPEN} from "../../services/Constants";
 import {errorToast, warningToast} from "../../services/ToastService";
 import LoadingDialog from "../LoadingDialog";
+import {style} from "../../assets/style/Custom";
 import {googleTrack} from "../../services/GoogleAnalytics";
 
 export default class SearchPage extends Component {
 
     constructor(props) {
         super(props);
-        googleTrack('Search Page', 'search page opened');
         this.state = {
             loading: true,
             tournaments: []
@@ -21,24 +21,28 @@ export default class SearchPage extends Component {
 
     init() {
         let data = {'status': TOURNAMENT_STATUS_OPEN};
+        googleTrack('Search Page', 'send request to get search result', 'status: '.data);
         search(data)
             .then((res) => {
                 this.setState({loading: false});
                 if (res.status === SUCCESS) {
                     this.setState({tournaments: res.data});
+                    googleTrack('Search Page', 'response of search tournament', res.message);
                 } else {
                     warningToast(res.message);
+                    googleTrack('Search Page', 'warning of search tournament', res.message);
                 }
             })
             .catch((error) => {
                 this.setState({loading: false});
                 errorToast(error.message);
+                googleTrack('Search Page', 'error of search tournament', error.message);
             });
     }
 
     render() {
         return (
-            <View style={{flex: 1}}>
+            <View style={style.flex}>
                 <LoadingDialog loading={this.state.loading} />
                 <TournamentsList loading={this.state.loading} tournaments={this.state.tournaments} />
             </View>
