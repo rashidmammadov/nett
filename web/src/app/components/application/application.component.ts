335043688c3c41d0c964e8141b53eb378dbd31b7
Rashid Mammadov <reshid@live.com>
2019-11-25 13:56:44 +0300

    Updated tabs visibility for user type.

--- web/src/app/components/application/application.component.ts
+++ web/src/app/components/application/application.component.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { first } from 'rxjs/operators';
+import { UserType } from '../../interfaces/user-type';
+import { TYPES } from '../../constants/types.constant';
 
+const HOME = {link: 'home', icon: 'home'};
+const BELONG = {link: 'belongs', icon: 'grid'};
+const SEARCH = {link: 'search', icon: 'search'};
+const SET_TOURNAMENT = {link: 'set-tournament', icon: 'plus-square'};
+const BALANCE = {link: 'balance', icon: 'credit-card'};
+const SETTINGS = {link: 'settings', icon: 'user'};
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
   styleUrls: ['./application.component.scss']
 })
 export class ApplicationComponent {
-    tabs: object[];
+    tabs: object[] = [];
+    user: UserType;
 
-    constructor() {
-        this.tabs = [
-            {link: 'home', icon: 'home'},
-            {link: 'belongs', icon: 'grid'},
-            {link: 'search', icon: 'search'},
-            {link: 'set-tournament', icon: 'plus-square'},
-            {link: 'balance', icon: 'credit-card'},
-            {link: 'settings', icon: 'user'},
-        ]
+    constructor(private store: Store<{user: UserType}>) {
+        this.getUserData().then();
     }
 
+    private getUserData = async () => {
+        this.user = await this.store.select('user').pipe(first()).toPromise();
+        this.prepareTabs(this.user.type);
+    };
+
+    private prepareTabs(userType: string) {
+        if (userType === TYPES.USER.HOLDER) {
+            this.tabs = [HOME, BELONG, SEARCH, SET_TOURNAMENT, SETTINGS];
+        } else if (userType === TYPES.USER.PLAYER) {
+            this.tabs = [HOME, BELONG, SEARCH, BALANCE, SETTINGS];
+        }
+    }
 }
